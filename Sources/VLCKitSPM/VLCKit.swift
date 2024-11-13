404a941334d9705ca8410668b88f50abeded77fa
Horizon <omkar@anthaathi.co>
2024-11-13 14:45:38 +0530

    Fixing for macCatalyst

--- Sources/VLCKitSPM/VLCKit.swift
+++ Sources/VLCKitSPM/VLCKit.swift
@@ -1,6 +1,6 @@
 #if os(tvOS)
 @_exported import TVVLCKit
-#elseif os(iOS) && !targetEnvironment(macCatalyst)
+#elseif os(iOS)
 @_exported import MobileVLCKit
 #elseif os(macOS)
 @_exported import VLCKit
